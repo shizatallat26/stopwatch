756ee24acd329602d2e051425f65d85b724caaab
shizatallat26 <shizatallat@gmail.com>
2021-03-11 23:39:13 +0500

    stopwatch

---
+++ app.js
@@ -0,0 +1,51 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+var interval;
+
+function timer(){
+    msec++;
+    msecHeading.innerHTML = msec;
+    if(msec >= 100){
+        sec++;
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }
+    else if(sec >= 60){
+        min++;
+        minHeading.innerHTML = min;
+        sec = 0;
+    }
+}
+
+function start(){
+    interval = setInterval(timer, 10);
+    document.getElementById("btnStart").disabled = true;
+    document.getElementById("btnStop").disabled = false;
+    document.getElementById("btnReset").disabled = false;
+}
+
+function stop(){
+    clearInterval(interval);
+    document.getElementById("btnStart").disabled = false;
+    document.getElementById("btnStop").disabled = true;
+    document.getElementById("btnReset").disabled = false;
+}
+
+function reset(){
+    min = 0;
+    sec = 0;
+    msec = 0;
+    minHeading.innerHTML = min;
+    secHeading.innerHTML = sec;
+    msecHeading.innerHTML = msec;
+    stop();
+    document.getElementById("btnStart").disabled = false;
+    document.getElementById("btnStop").disabled = false;
+    document.getElementById("btnReset").disabled = true;
+}
+
